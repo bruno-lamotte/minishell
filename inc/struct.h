f7475afda0f8b0fdabb9de66484a53d0f847834a
Bruno Lamotte <blamotte@f2r12s14.clusters.42paris.fr>
2026-02-25 00:20:18 +0100

    add idea of struct for parsing table generator

--- inc/struct.h
+++ inc/struct.h
@@ -9,3 +9,37 @@ typedef struct      s_token
     int             type;
     char            *value;
 }                   t_token;
+
+typedef struct      s_rule
+{
+    int             id;
+    char            *left_vars;
+    t_list          *right_vars;
+    int             nb_items;
+    struct s_rule   *next_rule;
+
+}                   t_rule;
+
+typedef struct      s_item
+{
+    int             id;
+    t_rule          rule_of_item;
+    int             dot_pos;
+    struct s_item   *next_item;
+}                   t_item;
+
+typedef struct      s_vars
+{
+    char            *var_name;
+    t_list          *var_firsts;
+    t_list          *var_follows;
+    struct s_cmd    *next_var;
+}                   t_vars;
+
+typedef struct      s_state
+{
+    int             id;
+    t_list          items;
+    int             reduction;
+    struct s_state  *next_state;
+}                   t_state;
