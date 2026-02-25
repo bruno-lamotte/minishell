082d74bb09e67fb6229979f8523267d74aa7a593
bruno-lamotte <96bronx69@gmail.com>
2026-02-25 07:17:52 +0100

    add t_transition: content for tlist in tstate correction errors in structs

--- inc/struct.h
+++ inc/struct.h
@@ -23,7 +23,7 @@ typedef struct      s_rule
 typedef struct      s_item
 {
     int             id;
-    t_rule          rule_of_item;
+    t_rule          *rule_of_item;
     int             dot_pos;
     struct s_item   *next_item;
 }                   t_item;
@@ -33,13 +33,21 @@ typedef struct      s_vars
     char            *var_name;
     t_list          *var_firsts;
     t_list          *var_follows;
-    struct s_cmd    *next_var;
+    struct s_vars    *next_var;
 }                   t_vars;
 
 typedef struct      s_state
 {
     int             id;
-    t_list          items;
-    int             reduction;
+    t_list          *items;
+    t_list          *transitions;
     struct s_state  *next_state;
 }                   t_state;
+
+typedef struct      s_transition
+{
+    char            *symbol;
+    struct s_state  *dest_state;
+}                   t_transition;
+
+#endif
