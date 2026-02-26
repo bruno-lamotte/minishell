8b13c5cddd8b65fbae77de2f3b8509adeadbcc25
bruno-lamotte <96bronx69@gmail.com>
2026-02-26 10:46:00 +0100

    add some utils in tokenizer

--- src/parsing/tokenizer/tokenizer.c
+++ src/parsing/tokenizer/tokenizer.c
@@ -13,10 +13,35 @@
 t_token make_token(char *str, int i)
 
 int is_end_of_imput(char c)
+{
+    return (c == '\0' || c == '\n');
+}
 
 int is_operator(char c)
+{
+    return (c == '|' || c == '<' || c == '>' || c == ';');
+}
 
 int can_be_added_to_opperator(char c)
+{
+    return (c == '<' || c == '>');
+}
+
+int is_blank(char c)
+{
+    return (c == ' ' || c == '\t');
+}
+
+int handle_comment(char *str, int *i)
+{
+    if (str[*i] == '#')
+    {
+        while (str[*i] && str[*i] != '\n')
+            (*i)++;
+        return (1);
+    }
+    return (0);
+}
 
 int handle_previus_is_operator(char *str, int *i)
 {
@@ -27,7 +52,7 @@ int handle_previus_is_operator(char *str, int *i)
     if (!is_operator(str[*i]))
         return (1);
     while (can_be_added_to_opperator(str[*i]))
-        *i++;
+        (*i)++;
     return (1);
 }
 
@@ -35,19 +60,18 @@ int handle_quote_and_backslash(char *str, int i)
 
 int handle_dollar_and_accent(char *str, int i)
 
-int is_blank(char c)
-
-int handle_comment(char *str, i)
-
 t_token tokenizer(char *str)
 {
     int     i;
 
+    while (is_blank(*str))
+        str++;
     i = 0;
     while(1)
     {
         if (is_end_of_imput(str[i]))
             return (make_token(str, i));
+        handle_comment(str, &i);
         if (handle_previus_is_operator(str, &i))
             return (make_token(str, i));
         if (handle_quote_and_backslash(str, &i))
@@ -56,11 +80,8 @@ t_token tokenizer(char *str)
             return (make_token(str, i));
         if (is_operator(str[i]) && i > 0)
             return (make_token(str, i - 1));
-        if (is_blank(str[i]) && i > 0)
+        if (is_blank(str[i]))
             return (make_token(str, i - 1));
-        else
-            str++;
-        handle_comment(str, &i);
         i++;
     }
 }
