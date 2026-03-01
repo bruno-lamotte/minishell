5bc55c0bdf219a895de22d88e55b56502e3e4a84
bruno-lamotte <96bronx69@gmail.com>
2026-03-01 23:09:29 +0100

    split tokenizer.c and add new files

---
+++ src/parsing/tokenizer/tokenizer_utils.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/01 20:32:21 by marvin            #+#    #+#             */
+/*   Updated: 2026/03/01 20:32:21 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int is_end_of_imput(char c)
+{
+    return (c == '\0' || c == '\n');
+}
+
+int is_operator(char c)
+{
+    return (c == '|' || c == '<' || c == '>' || c == ';');
+}
+
+int can_be_added_to_opperator(char c)
+{
+    return (c == '<' || c == '>');
+}
+
+int is_blank(char c)
+{
+    return (c == ' ' || c == '\t');
+}
+
+int handle_comment(char *str, int *i)
+{
+    if (str[*i] == '#')
+    {
+        while (str[*i] && str[*i] != '\n')
+            (*i)++;
+        return (1);
+    }
+    return (0);
+}
