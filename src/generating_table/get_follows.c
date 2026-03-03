439548f061b91c07b1c2bd8db079420ac3098725
Bruno Lamotte <blamotte@f4r6s1.clusters.42paris.fr>
2026-03-03 23:20:55 +0100

    add eof to start follows

--- src/generating_table/get_follows.c
+++ src/generating_table/get_follows.c
@@ -6,15 +6,29 @@
 /*   By: blamotte <blamotte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/27 14:34:18 by blamotte          #+#    #+#             */
-/*   Updated: 2026/02/27 16:01:25 by blamotte         ###   ########.fr       */
+/*   Updated: 2026/02/27 16:44:54 by blamotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+void add_eof_to_start_follow(t_parser *data)
+{
+    t_symbol    *symbol;
+    t_list      *new_list;
+    
+    symbol = get_symbol_from_rule(data, data->rules->content->left_symbol);
+    new_list = ft_lstnew("$");
+    if (!new_list)
+        return (/*JSP*/);
+    ft_lstadd_back(&symbol->follows, new_list);
+}
 
 void get_follows(t_parser *data)
 {
     t_list  *current_rule;
 
     current_rule = data->rules;
+    add_eof_to_follow(data);
+    lstget_symbol_from_rule(data, current_rule->content->left_symbol);
     while (current_rule)
     {
         
